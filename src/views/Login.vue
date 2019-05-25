7ab48b174c069ea34676f6a7e0304e1718ce3629
Alexandre Esquerdo Gonzalo <alexandreesquerdogonzalo@MBP-de-Alex.home>
2019-05-25 19:35:49 +0200

    login13

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -88,7 +88,7 @@
     </div>
     <v-footer height="auto" color="grey" class="footer_div">
       <v-layout justify-center row wrap>
-        <v-flex primary lighten-2 py-3 text-xs-center white--text xs12>
+        <v-flex py-3 text-xs-center white--text xs12>
           &copy;2019 —
           <strong>NAEGSA</strong>
         </v-flex>
